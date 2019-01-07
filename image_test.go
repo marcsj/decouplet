3853fed430b0ee045052cbcc585ecd34e2c4d8eb
Marc Johnson <marc@marcsj.com>
2019-01-06 22:23:03 -0800

    Added quick test for a much larger message to see performance.

--- image_test.go
+++ image_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -36,7 +37,7 @@ func TestImageMessage(t *testing.T) {
 		t.Error(err)
 	}
 	originalMessage :=
-		"!!**_-+Test THIS bigger message with More Symbols!" +
+			"!!**_-+Test THIS bigger message with More Symbols!" +
 			"@$_()#$%^#@!~#2364###$%! *(#$%)^@#%$@More and more"
 	newMessage, err := TranscodeImage([]byte(originalMessage), image)
 	if err != nil {
@@ -51,4 +52,37 @@ func TestImageMessage(t *testing.T) {
 		t.Fail()
 	}
 	t.Log("Message:", string(message))
+}
+
+func TestImageMessage_Image(t *testing.T) {
+	imageFile, err := os.Open("images/test.jpg")
+	if err != nil {
+		t.Error(err)
+	}
+	defer imageFile.Close()
+	fileInfo, err := imageFile.Stat()
+	if err != nil {
+		t.Error(err)
+	}
+	fileBytes := make([]byte, fileInfo.Size()) //fix this crap
+	_, err = imageFile.Read(fileBytes)
+	if err != nil {
+		t.Error(err)
+	}
+	image, err := LoadImage("images/test.jpg")
+	if err != nil {
+		t.Error(err)
+	}
+	newMessage, err := TranscodeImage(fileBytes, image)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("Length of message:", len(newMessage))
+	message, err := TransdecodeImage(newMessage, image)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(fileBytes) != string(message) {
+		t.Fail()
+	}
 }
