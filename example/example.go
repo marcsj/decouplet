e6d97f2082f4cf5f18b0c69a1efc5b9b7054b5b9
Marc Johnson <marc@marcsj.com>
2019-02-04 22:02:46 -0800

    Added example AES encryption and decryption.

--- example/example.go
+++ example/example.go
@@ -1,1 +1,41 @@
 package example
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func aesEncrypt(message []byte, key []byte) ([]byte, []byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, nil, err
+	}
+	encoded := aesgcm.Seal(nil, nonce, message, nil)
+	return encoded, nonce, nil
+}
+
+func aesDecrypt(input []byte, key []byte, nonce []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	plaintext, err := aesgcm.Open(nil, nonce, input, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
